68679d9891a4461e7d682cb648bd56dd833ec8bb
Sayan <sayanbanerjee2002@gmail.com>
2023-05-22 22:47:17 +0530

    added react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Login from "./Login/Login";
 import SignIn from "./Signin/SignIn";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      {/* <Login /> */}
-      <SignIn />
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/signup' element={<SignIn />} exact />
+      </Routes>
     </div>
   );
 }
